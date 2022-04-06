b305d192bae9b4289c2ea229a80afeacc317a5fa
Oliver Chico <oliverchiko2002@gmail.com>
2022-04-06 16:40:20 +0700

    add exp handler

--- Pokemon Battle Simulator/src/Pokemon/Pokemon.java
+++ Pokemon Battle Simulator/src/Pokemon/Pokemon.java
@@ -1,19 +1,24 @@
 package Pokemon;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class Pokemon {
+	protected final static int BASE_MAX_EXP = 10;
 	protected static int pokedexCounter = 0;
 	protected int pokedexNumber;
 	protected String name;
 	protected String type;
-	protected double baseHp;  // 1 - 1000
+	protected double baseHp; // 1 - 1000
 	protected double currHp;
 	protected double def; // 1 - 100
 	protected double att; // 1 - 100
+	protected int level;
+	protected int exp;
 	protected String weakAgainst;
 	protected String strongAgainst;
 	protected ArrayList<Move> moves = new ArrayList<Move>();
-	
+
 	public Pokemon(String name, double hp, double def, double att) {
 		pokedexCounter++;
 		this.pokedexNumber = pokedexCounter;
@@ -22,25 +27,54 @@ public class Pokemon {
 		this.currHp = hp;
 		this.def = def;
 		this.att = att;
+		this.level = this.exp = 0;
 	}
-	
-	public void addMove(Move move){
+
+	public void addMove(Move move) {
 		this.moves.add(move);
 	}
-	
-	
-//	Attack and Defence
+
+	// Level and Exp handler
+	// tambah exp
+	public void gainExp(int exp) {
+		this.exp += exp;
+		if (this.exp >= getNextLevelExp()) {
+			levelUp();
+		}
+	}
+
+	// buat dapetin max exp setiap tergantung level
+	public int getNextLevelExp() {
+		// level = 0, max_exp = 11
+		// level = 1, max_exp = 18
+		// level = 2, max_exp = 30
+		// level = 3, max_exp = 44
+		return (BASE_MAX_EXP + (level + 1)) * (level + 1) - (level * 3);
+	}
+
+	private void levelUp() {
+		exp = exp - getNextLevelExp();
+		level++;
+	}
+
+	// dapetin exp yang bakal didapetin kalo kalahin pokemon
+	public int getPokemonWorth() {
+		Random rand = new Random();
+		return level + rand.nextInt(level * 2);
+	}
+
+	// Attack and Defence
 	public void attack(Pokemon enemy, Move move) {
-		
+
 		double luck = Math.random();
-		if(luck > move.getChance()) {
+		if (luck > move.getChance()) {
 			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 			System.out.printf("%s Missed!\n", this.getName());
 			System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 			return;
 		}
-		
-		double totalDmg = move.getDmg() + (this.getAtt()*0.3);
+
+		double totalDmg = move.getDmg() + (this.getAtt() * 0.3);
 		enemy.defence(move, totalDmg);
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 		System.out.printf("%s attacked using %s!\n", this.getName(), move.getName());
@@ -48,29 +82,28 @@ public class Pokemon {
 		enemy.defence(move, totalDmg);
 		return;
 	}
-	
+
 	public void randomAttack(Pokemon enemy) {
-		int choice = (int) (Math.random()*3);
+		int choice = (int) (Math.random() * 3);
 		this.attack(enemy, this.getMoves().get(choice));
 	}
-	
+
 	public void defence(Move move, double damage) {
 		double totalDefence = this.def * 0.2;
-		if(this.getWeakAgainst().equals(move.getType())) {
+		if (this.getWeakAgainst().equals(move.getType())) {
 			System.out.println("Attack super effective!");
 			totalDefence *= 2;
 		}
-		if(this.getStrongAgainst().equals(move.getType())) {
+		if (this.getStrongAgainst().equals(move.getType())) {
 			System.out.println("Attack is not effective!");
 			totalDefence /= 2;
 		}
 		damage -= totalDefence;
-		this.setCurrHp(this.getCurrHp()- damage);
+		this.setCurrHp(this.getCurrHp() - damage);
 		return;
 	}
-	
-	
-//	Setter Getter
+
+	// Setter Getter
 
 	public static int getPokedexCounter() {
 		return pokedexCounter;
@@ -170,15 +203,14 @@ public class Pokemon {
 		System.out.println("--------------  Move List  -----------");
 		this.printMoves();
 		System.out.println("======================================");
-		
+
 	}
+
 	public void printMoves() {
-		for(int i = 0; i < moves.size(); i++) {
-			System.out.printf("Move[%d]", i+1);
+		for (int i = 0; i < moves.size(); i++) {
+			System.out.printf("Move[%d]", i + 1);
 			moves.get(i).printMove();
 		}
 	}
-	
-	
-	
+
 }
